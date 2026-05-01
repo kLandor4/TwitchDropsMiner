d07f338ff06a1b7e106eee972e8dcaacdce4fdd5
kLandor4 <123855177+kLandor4@users.noreply.github.com>
2026-04-30 22:53:11 -0500

    Update translate.py

--- translate.py
+++ translate.py
@@ -175,6 +175,7 @@ class GUISettingsAdvanced(TypedDict):
 
 
 class GUIPriorityModes(TypedDict):
+    priority_first: str
     priority_only: str
     ending_soonest: str
     low_availability: str
@@ -390,6 +391,7 @@ default_translation: Translation = {
                 "priority_only": "Priority list only",
                 "ending_soonest": "Ending soonest",
                 "low_availability": "Low availability first",
+                "priority_first": "Priority list first"
             },
             "game_name": "Game name",
             "priority": "Priority",
